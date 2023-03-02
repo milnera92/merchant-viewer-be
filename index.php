ab9a2c05a61dc1fd0d51c90aab59be9097e6b014
Andrew Milner <95140821+milnera92@users.noreply.github.com>
2023-03-02 13:27:10 -0500

    batch totals

--- index.php
+++ index.php
@@ -36,11 +36,16 @@ $terminals_result = mysqli_query($conn, $terminals_query);
 $transactions_query = "SELECT * FROM transactions";
 $transactions_result = mysqli_query($conn, $transactions_query);
 
+// Query terminal batch totals
+$batch_totals_query = "SELECT * FROM terminal_batch_totals";
+$batch_totals_result = mysqli_query($conn, $batch_totals_query);
+
 // Format data as JSON response
 $data = [
     "merchants" => [],
     "terminals" => [],
-    "transactions" => []
+    "transactions" => [],
+    "batch_totals" => []
 ];
 
 while ($row = mysqli_fetch_assoc($merchants_result)) {
@@ -53,6 +58,10 @@ while ($row = mysqli_fetch_assoc($terminals_result)) {
 
 while ($row = mysqli_fetch_assoc($transactions_result)) {
     $data["transactions"][] = $row;
+}
+
+while ($row = mysqli_fetch_assoc($batch_totals_result)) {
+    $data["batch_totals"][] = $row;
 }
 
 header('Content-Type: application/json');
